7afca90cc6f059e9f6816f7f8535ec3a3e2bb2d0
choiyoung0802 <mj12579632@gmail.com>
2026-03-31 16:05:39 +0900

    mypage

--- frontend/trip-planner/src/components/router/Router.tsx
+++ frontend/trip-planner/src/components/router/Router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "../../pages/MainPage";
 import LoginPage from "../../pages/LoginPage";
+import MyPage from "../../pages/MyPage";
 
 export default function Router() {
   return (
@@ -8,6 +9,7 @@ export default function Router() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </BrowserRouter>
   );
