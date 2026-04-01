24f3e2afe1a5b9e566e1976d96ff17045f925716
Karas02000 <gta5369577@naver.com>
2026-04-01 15:00:13 +0900

    feat: frontend signup created and bug fix

--- frontend/trip-planner/src/components/router/Router.tsx
+++ frontend/trip-planner/src/components/router/Router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "../../pages/MainPage";
 import LoginPage from "../../pages/LoginPage";
+import SignupPage from "../../pages/SignupPage";
 import MyPage from "../../pages/MyPage";
 
 
@@ -10,6 +11,7 @@ export default function Router() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </BrowserRouter>
